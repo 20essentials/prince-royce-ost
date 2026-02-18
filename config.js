eab3d8928dd9420b81a07f71c8a5444cd947d621
DominicDev <s0l0estudi4r@gmail.com>
2026-02-18 18:18:23 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PRINCE ROYCE';
+export let nameSongs = [
+	"STAND BY ME", /*1*/
+	"DARTE UN BESO", /*2*/
+	"CORAZÓN SIN CARA", /*3*/
+	"RECHAZAME", /*4*/
+	"EL AMOR QUE PERDIMOS", /*5*/
+	"TE ROBARE", /*6*/
+	"LAS COSAS PEQUEÑAS", /*7*/
+	"INCONDICIONAL", /*8*/
+	" SOY EL MISMO", /*9*/
+	"MI ULTIMA CARTA", /*10*/
+	"LA CARRETERA", /*11*/
+	"DÉJÀ VU", /*12*/
+	"DILEMA", /*13*/
+	"TE NECESITO", /*14*/
+	"PRINCE ROYCE", /*15*/
+	"NO CREO EN EL AMOR", /*16*/
+	"LIBERAME", /*17*/
+	" NO TE OLVIDES", /*18*/
+	"SOLITA", /*19*/
+	"TE ME VAS", /*20*/
+]
